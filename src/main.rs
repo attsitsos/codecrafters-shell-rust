bdc1740eed5dcd55221c1f23256195184fe68d1b
thanos93 <thanasis.tsitsos4@gmail.com>
2024-10-31 16:31:51 +0200

    REPL #1

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
     // Wait for user input
     loop {
 
+        print!("$ ");
+        io::stdout().flush().unwrap();
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
