267e74b17b79a12da82cf4a2fddbcb08c1df123b
thanos93 <thanasis.tsitsos4@gmail.com>
2024-10-31 19:50:25 +0200

    type command searching to the PATH environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::env;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::Path;
 use std::process;
 
 fn print_bash_icon() {
@@ -10,13 +12,22 @@ fn command_not_found(command: &str) {
     println!("{}: not found", command);
 }
 
-fn type_command(args: Option<&str>) {
+fn type_command(args: Option<&str>, path: &str) {
     match args {
         Some("exit") => println!("exit is a shell builtin"),
         Some("echo") => println!("echo is a shell builtin"),
         Some("type") => println!("type is a shell builtin"),
         None => eprintln!("please specify your command"),
-        Some(c) => command_not_found(c),
+        Some(c) => {
+            for sub_path in path.split(":") {
+                let p = Path::new(sub_path).join(c);
+                if p.exists() {
+                    println!("{:?}", p);
+                    return;
+                }
+            }
+            command_not_found(c);
+        }
     }
 }
 
@@ -27,21 +38,26 @@ fn echo_command(args: Option<&str>) {
     }
 }
 
-fn process_command(full_command: &str) {
+fn process_command(full_command: &str, path: &str) {
     let mut split = full_command.splitn(2, ' ');
     let command = split.next();
     let args = split.next();
     match command {
         Some("exit") => process::exit(0),
         Some("echo") => echo_command(args),
-        Some("type") => type_command(args),
+        Some("type") => type_command(args, path),
         Some(c) => command_not_found(c),
         None => print_bash_icon(),
     }
 }
 
 fn main() {
-    // Wait for user input
+    let path = env::var("PATH");
+    if let Err(e) = path {
+        eprintln!("cannot found PATH env variable: {}", e);
+        process::exit(1);
+    }
+    let path = path.unwrap();
     loop {
         print_bash_icon();
         io::stdout().flush().unwrap();
@@ -51,6 +67,6 @@ fn main() {
             eprintln!("\rstdin: failed to read. {}", error);
             process::exit(1);
         }
-        process_command(input.trim());
+        process_command(input.trim(), &path);
     }
 }
