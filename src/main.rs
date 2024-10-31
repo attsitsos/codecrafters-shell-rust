e2517fde2db123fb09fc94a1164d867a3c5e16c6
thanos93 <thanasis.tsitsos4@gmail.com>
2024-10-31 18:37:18 +0200

    Added echo buildin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
+
+fn process_command(full_command: &str) {
+    let mut split = full_command.splitn(2, ' ');
+    let command = split.next();
+    let args = split.next();
+    match command {
+        Some("exit") => process::exit(0),
+        Some("echo") => if let Some(a) = args { println!("{}", a); } else { println!("");},
+        Some(c) =>  println!("{}: command not found", c),
+        None => println!("$ "),
+    }
+}
 
 fn main() {
     // Wait for user input
@@ -10,10 +23,6 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        if input.trim() == "exit 0" {
-            break;
-        }
-        println!("{}: command not found", input.trim());
-
+        process_command(input.trim());
     }
 }
